6782d0d2950ce90c9539472823d5f399826fd47d
akhilreddy13UCM <axe55580@ucmo.edu>
2025-02-07 11:10:15 -0600

    Git owner rent2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Git owner owner owner" );
+        System.out.println( "Git owner rent2" );
         System.out.println( "Hello World!" );
     }
 }
