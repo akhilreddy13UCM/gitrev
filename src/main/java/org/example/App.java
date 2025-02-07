08eae3fc5608e132feace626f20ae48da6644466
akhilreddy13UCM <axe55580@ucmo.edu>
2025-02-07 11:15:40 -0600

    Git owner real estate

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println("Git Real estate");
         System.out.println( "Git owner " );
         System.out.println( "Hello World!" );
     }
