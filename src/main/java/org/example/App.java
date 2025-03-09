08ff9ace8aacd8b04080a1133a85ef7e88ae6577
akhilreddy13UCM <axe55580@ucmo.edu>
2025-03-09 13:03:40 -0500

    Helooooo3331111

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,11 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        
-        System.out.println( "Heloooooo World!11111" );
-        System.out.println( "Heloooooo World!333" );
-
-        System.out.println( "Heloooooo World!3333" );
-        System.out.println( "Heloooooo World!444" );
+        System.out.println( "AAAAAAA" );
     }
 }
